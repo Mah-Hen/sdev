4b895b8f08e0f14cf57c9c6eb06981efac895c35
Mahlaki <mahlaki14@gmail.com>
2023-02-06 20:02:30 -0500

    new add

---
+++ Java/Bikes/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String args[]) {
+        MountainBike mb = new MountainBike(3, 100, 25);
+        mb.speedUp(2);
+        System.out.println(mb.toString());
+        MountainBike b2 = new MountainBike(3, 100, 25);
+        if (mb.equals(b2))
+            System.out.println("equal");
+        else
+            System.out.println("not equal");
+    }
+}
